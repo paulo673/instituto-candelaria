ef06bd27621f65c59b77ac23623b136d3c873edf
Paulo <Paulo@DESKTOP-FT12GQD>
2021-03-04 01:00:04 -0400

    criado setup do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
+    font: 400 16px Roboto, sans-serif;
+  }
+`
